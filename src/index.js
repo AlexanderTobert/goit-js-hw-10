5715bbd1c3ec213645eeff01687cbb8f1e38ec8b
AlexanderTobert <tobert.sasha@gmail.com>
2022-10-03 23:43:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,24 +23,15 @@ refs.searchInput.addEventListener('input', debounce(() => {
 }, DEBOUNCE_DELAY))
 
 function showCoutry(countryInfo) {
-    //{ name, capital, population, flags, languages, length}
     reset();
-    if (countryInfo.length > 10) {
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    } else if (countryInfo.length >= 2) {
-        countryInfo.map(item => {
-            const countryListItem = `<li style="list-style:none; margin:5px"><img src="${item.flags.svg}" width="30" height="20" /> ${item.name.official}</li>`
-            refs.countryList.insertAdjacentHTML('beforeend', countryListItem);
-        })
+    if (countryInfo.length === 1) {
+        renderCountryInfo(countryInfo);
+        return;
+    }
+    if (countryInfo.length >= 2 && countryInfo.length <= 10) {
+        renderList(countryInfo);
     } else {
-        const countryData = `<span style="font-size:28px; font-family:bold">
-        <img src="${countryInfo[0].flags.svg}" width="35" height="35" /> 
-        ${countryInfo[0].name.official}
-        </span>
-        <p>Capital: ${countryInfo[0].capital}</p>
-        <p>Population: ${countryInfo[0].population}</p>
-        <p>Languages: ${Object.values(countryInfo[0].languages)}</p>`;
-        refs.countryContainer.innerHTML = countryData;
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     }
 }
 
@@ -53,4 +44,22 @@ function showError(error) {
 function reset() {
     refs.countryList.innerHTML = '';
     refs.countryContainer.innerHTML = '';
+}
+
+function renderList(countryInfo) {
+    countryInfo.map(item => {
+            const countryListItem = `<li style="list-style:none; margin:5px; padding:0"><img src="${item.flags.svg}" width="30" height="20" /> ${item.name.official}</li>`
+            refs.countryList.insertAdjacentHTML('beforeend', countryListItem);
+        })
+}
+
+function renderCountryInfo(countryInfo) {
+    const countryData = `<span style="font-size:28px; font-family:bold">
+        <img src="${countryInfo[0].flags.svg}" width="35" height="35" /> 
+        ${countryInfo[0].name.official}
+        </span>
+        <p>Capital: ${countryInfo[0].capital}</p>
+        <p>Population: ${countryInfo[0].population}</p>
+        <p>Languages: ${Object.values(countryInfo[0].languages)}</p>`;
+        refs.countryContainer.insertAdjacentHTML('afterbegin', countryData);
 }
